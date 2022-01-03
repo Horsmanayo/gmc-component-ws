e5b80852ea496965b4d1050c0abc0a3c75a59800
Horsmanayo <oniademola001@gmail.com>
2022-01-03 17:43:27 +0100

    first commit

---
+++ src/Component/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+    return (
+        <div>
+            <img src='/raycast-untitled.png' alt='myimage' className='imago' />
+        </div>
+    )
+}
+
+export default ProfilePhoto
